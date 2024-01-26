a246e4155255c8dc03fcdc61ca2b0a903d36b03a
akhil <akhil.bindhu@outlook.com>
2024-01-27 00:08:38 +0530

    Initial commit

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -8,28 +8,95 @@ import { Component, OnInit } from '@angular/core';
 export  class DashboardComponent implements OnInit {
 
   Designation:string="";
-  UserName:string="";
-  NoOfTeamMemebers:number=0;
+  Username:string="";
+  NoOfTeamMembers:number=0;
   TotalCostOfAllProjects:number=0;
   PendingTasks:number=0;
   UpComingProjects:number=0;
   ProjectCost:number=0;
   CurrentExpenditure:number=0;
   AvailableFunds:number=0;
+  Clients:string[]=[];
+  Projects:string[]=[];
+  Years:number[]=[];
+  TeamMembers:any =[];
+  Regions:any =[];
+
 
 
 
 
   ngOnInit(){
     this.Designation="Project Manager";
-    this.UserName="Akhil";
-    this.NoOfTeamMemebers=23;
+    this.Username="Akhil";
+    this.NoOfTeamMembers=23;
     this.TotalCostOfAllProjects=200000;
     this.PendingTasks=12;
     this.UpComingProjects=45;
     this.CurrentExpenditure=1300000;
     this.AvailableFunds=5416;
     this.ProjectCost=200000;
+    this.Clients=[
+      "WhiteBlue",
+      "BNY MELLON",
+      "Your Combo"
+    ];
+
+    this.Projects=[
+      "Project A",
+      "Project B",
+      "Project C"
+
+    ];
+    for(var i=2019;i>=2010;i--){
+      this.Years.push(i);
+    }
+    this.TeamMembers=[
+      {Region:"East",TeamMembersCount:20,AvailableMembers:3},
+      {Region:"West",TeamMembersCount:20,AvailableMembers:3},
+      {Region:"North",TeamMembersCount:20,AvailableMembers:3},
+      {Region:"South",TeamMembersCount:20,AvailableMembers:3},
+      
+    ];
+    this.Regions=[
+      {Direction:"East",
+      Members:[
+        {id:1,name:"Akhil",status:"Avaiable"},
+        {id:2,name:"Akhil",status:"Avaiable"},
+        {id:3,name:"Akhil",status:"Avaiable"},
+        {id:4,name:"Akhil",status:"Avaiable"},
+        {id:5,name:"Akhil",status:"Avaiable"}
+      ]
+      },
+      {Direction:"South",
+      Members:[
+        {id:1,name:"Akhil",status:"Avaiable"},
+        {id:2,name:"Akhil",status:"Avaiable"},
+        {id:3,name:"Akhil",status:"Avaiable"},
+        {id:4,name:"Akhil",status:"Avaiable"},
+        {id:5,name:"Akhil",status:"Avaiable"}
+      ]
+      },
+      {Direction:"West",
+      Members:[
+        {id:1,name:"Akhil",status:"Avaiable"},
+        {id:2,name:"Akhil",status:"Avaiable"},
+        {id:3,name:"Akhil",status:"Avaiable"},
+        {id:4,name:"Akhil",status:"Avaiable"},
+        {id:5,name:"Akhil",status:"Avaiable"}
+      ]
+      },
+      {Direction:"North",
+      Members:[
+        {id:1,name:"Akhil",status:"Avaiable"},
+        {id:2,name:"Akhil",status:"Avaiable"},
+        {id:3,name:"Akhil",status:"Avaiable"},
+        {id:4,name:"Akhil",status:"Avaiable"},
+        {id:5,name:"Akhil",status:"Avaiable"}
+      ]
+      }
+    ]
+
   }
 
 }
